a733cd03767adfef3a1292b46602b67fb04879df
Jai Prakash <jaiprakash101072@gmail.com>
2025-05-16 11:11:54 +0530

    done

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import Documents from './components/Documents'
+
+const Page = () => {
+    return (
+        <div>
+            <Documents />
+        </div>
+    )
+}
+
+export default Page;
